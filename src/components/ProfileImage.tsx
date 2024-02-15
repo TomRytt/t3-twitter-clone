2a50bc4589b99753cd81e40900ea4ba608d1e0e0
Tom <tomofekrytt@gmail.com>
2024-02-15 23:17:39 +0200

    added new tweet, button and profile image tsx

---
+++ src/components/ProfileImage.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+type ProfileImageProps = {
+  src?: string | null;
+  className?: string;
+};
+
+export function ProfileImage({ src, className = "" }: ProfileImageProps) {
+  return (
+    <div
+      className={`relative-position h-12 w-12 overflow-hidden rounded-full ${className}`}
+    >
+      {src == null ? null : (
+        <Image
+          src={src}
+          alt="Profile Image"
+          quality={100}
+          width={100}
+          height={100}
+        />
+      )}
+    </div>
+  );
+}
