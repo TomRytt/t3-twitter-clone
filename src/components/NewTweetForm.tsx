59773883971a3af7ddb85054e959ffec7b82d52f
Tom <tomofekrytt@gmail.com>
2024-02-24 19:42:54 +0200

    automatically refresh on new tweet

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -27,6 +27,7 @@ function Form() {
   const session = useSession();
   const [inputValue, setInputValue] = useState("");
   const textAreaRef = useRef<HTMLTextAreaElement>();
+  const trpcUtils = api.useUtils();
 
   const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
     updateTextAreaSize(textArea);
@@ -39,8 +40,36 @@ function Form() {
 
   const createTweet = api.tweet.create.useMutation({
     onSuccess: (newTweet) => {
-      console.log(newTweet);
       setInputValue("");
+
+      if (session.status !== "authenticated") return;
+
+      trpcUtils.tweet.infiniteFeed.setInfiniteData({}, (oldData) => {
+        if (oldData?.pages[0] == null) return;
+
+        const newCacheTweet = {
+          ...newTweet,
+          likeCount: 0,
+          likedByMe: false,
+          user: {
+            id: session.data.user.id,
+            name: session.data.user.name ?? null,
+            image: session.data.user.image ?? null,
+          },
+        };
+
+        return {
+          ...oldData,
+          pages: [
+            {
+              ...oldData.pages[0],
+              tweets: [newCacheTweet, ...oldData.pages[0].tweets],
+            },
+
+            ...oldData.pages.slice(1),
+          ],
+        };
+      });
     },
   });
 
