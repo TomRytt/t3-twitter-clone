a01c46ebf62dc4285b3b1bf7aa6ec54f89712d7b
Tom Rytt <tomofekrytt@gmail.com>
2024-02-27 21:14:17 +0200

    started working on profile page cmp

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -1,3 +1,34 @@
-export default function id() {
-  return <h1>Id</h1>;
+import type { NextPage, GetStaticPropsContext } from "next";
+import { redirect } from "next/dist/server/api-utils";
+import Head from "next/head";
+import { ssgHelper } from "~/server/api/ssgHelper";
+
+const ProfilePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <h1>TBC</h1>
+        {/* <title>{`Twitter Clone ${user.name} `}</title> */}
+      </Head>
+    </>
+  );
+};
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  const id = context.params?.id;
+
+  if (id == null) {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+
+  const ssg = ssgHelper();
+
+  // TODO: Continue at 1:57 on the video and create the profile router
+  // ssg.profile
 }
+
+export default ProfilePage;
