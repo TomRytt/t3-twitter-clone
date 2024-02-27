bee0532553c8e95b5eb90fa4a3ed570707685fe1
Tom Rytt <tomofekrytt@gmail.com>
2024-02-27 20:47:22 +0200

    added onlyFollowing and moved getInfiniteTweets to separate func

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -14,53 +14,22 @@ import type {
 export const tweetRouter = createTRPCRouter({
     infiniteFeed: publicProcedure.input(
         z.object({
+            onlyFollowing: z.boolean().optional(),
             limit: z.number().optional(),
             cursor: z.object({
                 id: z.string(),
                 createdAt: z.date()
             }).optional()
         }),
-    ).query(async ({ input: { limit = 10, cursor }, ctx }) => {
-        const currentUserId = ctx.session?.user.id;
-
-        const data = await ctx.db.tweet.findMany({
-            take: limit + 1,
-            cursor: cursor ? { id: cursor.id } : undefined,
-            orderBy: [{ createdAt: "desc" }, { id: "desc" }],
-            select: {
-                id: true,
-                content: true,
-                createdAt: true,
-                _count: { select: { likes: true } },
-                likes: currentUserId == null ? false : { where: { userId: currentUserId } },
+    ).query(async ({ input: { limit = 10, onlyFollowing = false, cursor }, ctx }) => {
+        const currentUserId = ctx.session?.user.id
+        return await getInfiniteTweets({
+            limit, ctx, cursor, whereClause: currentUserId == null || !onlyFollowing ? undefined : {
                 user: {
-                    select: { name: true, id: true, image: true }
+                    followers: { some: { id: currentUserId } }
                 }
             }
         })
-
-        let nextCursor: typeof cursor | undefined
-
-        if (data.length > limit) {
-            const nextItem = data.pop()
-
-            if (nextItem != null) {
-                nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt }
-            }
-        }
-
-        return {
-            tweets: data.map(tweet => {
-                return {
-                    id: tweet.id,
-                    content: tweet.content,
-                    createdAt: tweet.createdAt,
-                    likeCount: tweet._count.likes,
-                    user: tweet.user,
-                    likedByMe: tweet.likes?.length > 0
-                }
-            }), nextCursor
-        }
     }),
 
     create: protectedProcedure
@@ -87,3 +56,55 @@ export const tweetRouter = createTRPCRouter({
     })
 
 });
+
+
+async function getInfiniteTweets({ whereClause, ctx, limit, cursor }:
+    {
+        whereClause?: Prisma.TweetWhereInput,
+        limit: number,
+        cursor:
+        { id: string, createdAt: Date } | undefined,
+        ctx: inferAsyncReturnType<typeof createTRPCContext>
+    }) {
+    const currentUserId = ctx.session?.user.id;
+
+    const data = await ctx.db.tweet.findMany({
+        take: limit + 1,
+        cursor: cursor ? { id: cursor.id } : undefined,
+        orderBy: [{ createdAt: "desc" }, { id: "desc" }],
+        where: whereClause,
+        select: {
+            id: true,
+            content: true,
+            createdAt: true,
+            _count: { select: { likes: true } },
+            likes: currentUserId == null ? false : { where: { userId: currentUserId } },
+            user: {
+                select: { name: true, id: true, image: true }
+            }
+        }
+    })
+
+    let nextCursor: typeof cursor | undefined
+
+    if (data.length > limit) {
+        const nextItem = data.pop()
+
+        if (nextItem != null) {
+            nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt }
+        }
+    }
+
+    return {
+        tweets: data.map(tweet => {
+            return {
+                id: tweet.id,
+                content: tweet.content,
+                createdAt: tweet.createdAt,
+                likeCount: tweet._count.likes,
+                user: tweet.user,
+                likedByMe: tweet.likes?.length > 0
+            }
+        }), nextCursor
+    }
+}
