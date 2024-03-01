185c6366e29b3b8255f63bb50c952602ae43affc
Tom Rytt <tomofekrytt@gmail.com>
2024-03-01 11:40:48 +0200

    added most of the profile page logic- likes, tweets, following

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -1,35 +1,132 @@
-import type { NextPage, GetStaticPaths } from "next";
-import { redirect } from "next/dist/server/api-utils";
+import type {
+  NextPage,
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from "next";
 import Head from "next/head";
 import { ssgHelper } from "~/server/api/ssgHelper";
+import { api } from "~/utils/api";
+import ErrorPage from "next/error";
+import Link from "next/link";
+import { IconHoverEffect } from "~/components/IconHoverEffect";
+import { VscArrowLeft } from "react-icons/vsc";
+import { ProfileImage } from "~/components/ProfileImage";
+import { InfiniteTweetList } from "~/components/InfiniteTweetList";
+import { useSession } from "next-auth/react";
+import { Button } from "~/components/Button";
 
-const ProfilePage: NextPage = () => {
+const ProfilePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  id,
+}) => {
+  const { data: profile } = api.profile.getById.useQuery({ id });
+  const tweets = api.tweet.infiniteProfileFeed.useInfiniteQuery(
+    { userId: id },
+    { getNextPageParam: (lastPage) => lastPage.nextCursor },
+  );
+
+  if (profile?.name == null) {
+    return <ErrorPage statusCode={404} />;
+  }
+
   return (
     <>
       <Head>
         <h1>TBC</h1>
-        {/* <title>{`Twitter Clone ${user.name} `}</title> */}
+        <title>{`Twitter Clone - ${profile.name} `}</title>
       </Head>
+      <header className="sticky top-0 z-10 flex items-center border-b bg-white px-4 py-2">
+        <Link href=".." className="mr-2">
+          <IconHoverEffect>
+            <VscArrowLeft className="size-6" />
+          </IconHoverEffect>
+        </Link>
+        <ProfileImage src={profile.image} className="flex-shrink-0" />
+        <div className="ml-2 flex-grow">
+          <h1 className="text-lg font-bold">{profile.name}</h1>
+          <div className="text-gray-500">
+            {profile.tweetsCount}{" "}
+            {getPlural(profile.tweetsCount, "Tweet", "Tweets")} -{" "}
+            {profile.followersCount}{" "}
+            {getPlural(profile.followersCount, "Follower", "Followers")} -{" "}
+            {`${profile.followsCount} Following`}
+          </div>
+        </div>
+        <FollowButton
+          isFollowing={profile.isFollowing}
+          userId={id}
+          onClick={() => null}
+        />
+      </header>
+      <main>
+        <InfiniteTweetList
+          tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
+          isError={tweets.isError}
+          isLoading={tweets.isLoading}
+          hasMore={tweets.hasNextPage}
+          fetchNewTweets={tweets.fetchNextPage}
+        />
+      </main>
     </>
   );
 };
 
-// TODO: Fix this function
-// export async function getStaticProps(context: GetStaticPaths) {
-//   const id = context.params?.id;
+type FollowButtonProps = {
+  isFollowing: boolean;
+  userId: string;
+  onClick: () => void;
+};
 
-//   if (id == null) {
-//     return {
-//       redirect: {
-//         destination: "/",
-//       },
-//     };
-//   }
+function FollowButton({ userId, isFollowing, onClick }: FollowButtonProps) {
+  const session = useSession();
+  if (session.status !== "authenticated" || session.data.user.id == userId) {
+    return null;
+  }
 
-//   const ssg = ssgHelper();
+  return (
+    <Button onClick={onClick} small gray={isFollowing}>
+      {isFollowing ? "Unfollow" : "Follow"}
+    </Button>
+  );
+}
 
-//   // TODO: Continue at 1:57 on the video and create the profile router
-//   // ssg.profile
-// }
+const pluralRules = new Intl.PluralRules();
+function getPlural(number: number, singular: string, plural: string) {
+  return pluralRules.select(number) === "one" ? singular : plural;
+}
+
+export const getStaticPaths: GetStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export async function getStaticProps(
+  context: GetStaticPropsContext<{ id: string }>,
+) {
+  const id = context.params?.id;
+
+  if (id == null) {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+
+  const ssg = ssgHelper();
+  await ssg.profile.getById.prefetch({ id });
+
+  return {
+    props: {
+      id,
+      trpcState: ssg.dehydrate(),
+    },
+  };
+
+  // TODO: Continue at 1:57 on the video and create the profile router
+  // ssg.profile
+}
 
 export default ProfilePage;
